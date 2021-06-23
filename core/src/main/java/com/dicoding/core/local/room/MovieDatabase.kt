69c5c9755124025db5f846393bbdb572635da3f5
BryanRJR <bryanrantung98@gmail.com>
2021-06-23 13:09:04 +0800

    commit 4

--- core/src/main/java/com/dicoding/core/local/room/MovieDatabase.kt
+++ core/src/main/java/com/dicoding/core/local/room/MovieDatabase.kt
@@ -3,7 +3,7 @@ package com.dicoding.core.local.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [com.dicoding.core.local.entity.MovieEntity::class], version = 3, exportSchema = false)
+@Database(entities = [com.dicoding.core.local.entity.MovieEntity::class], version = 2, exportSchema = false)
 abstract class MovieDatabase : RoomDatabase() {
 
     abstract fun movieDao(): MovieDao
